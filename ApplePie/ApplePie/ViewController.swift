3320c664b096b10a2562ad29c1b2fc0b8c0f8be7
nastyabekesheva <nastya.bekesheva@gmail.com>
2022-03-17 22:10:16 +0200

    Part One

---
+++ ApplePie/ApplePie/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  ApplePie
+//
+//  Created by Nastya Bekesheva on 17.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var treeImageView: UIImageView!
+    @IBOutlet var correctWordLabel: UILabel!
+    @IBOutlet var scoreLabel: UILabel!
+    @IBOutlet var letterButtons: [UIButton]!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func letterButtonPressed(_ sender: UIButton) {
+        sender.isEnabled = false
+    }
+    
+    
+    
+    
+
+}
+
