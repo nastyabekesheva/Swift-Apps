bf3afcfc98d5b0346f9f33814f183e7a49476475
nastyabekesheva <nastya.bekesheva@gmail.com>
2022-03-19 18:10:20 +0200

    Part Three

--- ApplePie/ApplePie/ViewController.swift
+++ ApplePie/ApplePie/ViewController.swift
@@ -27,12 +27,16 @@ class ViewController: UIViewController {
 
     @IBAction func letterButtonPressed(_ sender: UIButton) {
         sender.isEnabled = false
+        let letterString = sender.configuration!.title!
+        let letter = Character(letterString.lowercased())
+        currentGame.playerGuessed(letter: letter)
+        updateUI()
     }
     
     func newRound(){
         let newWord = listOfWords.removeFirst()
             currentGame = Game(word: newWord, incorrectMovesRemaining:
-            incorrectMovesAllowed)
+            incorrectMovesAllowed, guessedLetters: [])
             updateUI()
     }
     
